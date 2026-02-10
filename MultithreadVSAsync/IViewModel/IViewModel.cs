45eac67a5cd417da7e2d88a8a6fbecb49fb934aa
faritietl <luca.faritiet@viacesi.fr>
2026-02-10 15:18:56 +0100

    Struct: Basis project showing the diff between Async and Multithreading mtds

---
+++ MultithreadVSAsync/IViewModel/IViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MultithreadVSAsync.IViewModel
+{
+    internal interface IViewModel
+    {
+        public abstract void Start();
+        public static abstract void Choose();
+        public static abstract void ApplyChoice(string choice);
+    }
+}
---
+++ MultithreadVSAsync/ViewModel/ViewModel.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MultithreadVSAsync.Model;
+
+namespace MultithreadVSAsync.ViewModel
+{
+    internal class ViewModel : IViewModel.IViewModel
+    {
+        public static void Choose()
+        {
+            Console.WriteLine("Welcome to this Async vs Multithreadin comparaison.");
+            Console.WriteLine("Please choose the method you want to test :");
+            Console.WriteLine("1 - Multithreading");
+            Console.WriteLine("2 - Asynchronisme");
+            Console.WriteLine("3 - Exit");
+            Console.Write("Your choice: ");
+        }
+        public static void ApplyChoice(string choice)
+        {
+            switch (choice)
+            {
+                case "0":
+                    Choose();
+                    break;
+                case "1":
+                    Console.Clear();
+                    Multithread.MultithreadMethod();
+                    break;
+                case "2":
+                    Console.Clear();
+                    Asynchronisme.AsynchronismeMethod();
+                    break;
+                case "3":
+                    Console.Clear();
+                    Console.WriteLine("Goodbye !");
+                    break;
+                default:
+                    Console.Clear();
+                    Console.WriteLine("Invalid choice, please try again.");
+                    Choose();
+                    break;
+            }
+        }
+        public void Start()
+        {
+            string choice = "0";
+            while (choice != "1" && choice != "2" && choice != "3") {
+                ApplyChoice(choice);
+                choice = Console.ReadLine();
+            }
+            ApplyChoice(choice);
+
+        }
+    }
+}
